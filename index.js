4a906699c122bea09e06dcdd357069f463686d6f
xiaoshidu <xiaoshidu@gmail.com>
2015-02-17 03:23:15 +0000

    update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var app = express();
 var fs = require('fs');
 
 var data = fs.readFileSync('/home/ubuntu/bitstarter/index.html','utf8');
+var buf = Buffer(data)
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   //response.send('Hello World2!');
-  response.send(data);
+  response.send(buf.toString('utf8'));
 })
 
 app.listen(app.get('port'), function() {
